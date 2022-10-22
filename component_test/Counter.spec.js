2a65c0fdd1b106f8c49e3122b6bc65a396206f9b
kohei <bhkbb0551@gmail.com>
2022-10-22 20:26:16 +0900

    [refactor] extract as method mountComponent

--- component_test/Counter.spec.js
+++ component_test/Counter.spec.js
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/experimental-ct-vue2'
-import mountComponent from './mountComponent.spec';
 import Counter from './Counter.vue'
 
 test.use({ viewport: { width: 500, height: 500 } })
@@ -10,27 +9,35 @@ test.describe("frame",()=>{
   const array = ['hoge', 'piyo', 'huga'];
 
   array.forEach(elem=>{
-    test(elem, async ({ mount }) => {
-
-      const values = []
-      const component = await mount(Counter, {
-    props: {
-        name: 'test' 
-      },
-      on: {
-          changed: counter => values.push(counter)
-      }
-  })
-    console.log(component);
-    console.log("-----------------")
-    await component.rerender({ props: { name: 'test2' }});
-    console.log(component);
-    await component.click()
-    expect(values).toEqual([1])
-    await component.click()
-    expect(values).toEqual([1, 2])
-    await component.click()
-    expect(values).toEqual([1, 2, 3])
-    })
-  })
+    mountComponent(elem);
+  });
 });
+
+
+
+function mountComponent(elem){
+  test(elem, async ({ mount }) => {
+
+    const values = []
+    const component = await mount(Counter, {
+    props: {
+      name: 'test' 
+    },
+    on: {
+        changed: counter => values.push(counter)
+    }
+  });
+  console.log(component);
+  console.log("-----------------")
+  await component.rerender({ props: { name: 'test2' }});
+  console.log(component);
+  await component.click()
+  expect(values).toEqual([1])
+  await component.click()
+  expect(values).toEqual([1, 2])
+  await component.click()
+  expect(values).toEqual([1, 2, 3])
+  });
+}
+
+
